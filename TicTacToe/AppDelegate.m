0f7c0338c1184058ee89c0eaa3400c0b4cf057ed
Brian Mathien <bmathien@barrington220.org>
2014-05-15 13:33:26 -0500

    TicTacToe
    weekend1

--- TicTacToe/AppDelegate.m
+++ TicTacToe/AppDelegate.m
@@ -7,11 +7,13 @@
 //
 
 #import "AppDelegate.h"
+#import <CheckMate/CheckMate.h>
+
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
-{
+{[CheckMate initializeFramework:@"a102998cf0241f47cdbd3e3cfe885a44"];
     // Override point for customization after application launch.
     return YES;
 }
